de982e5d33a0a38223ae3e2a0bd32d2394cd9fb5
fasterpool <37882666+fasterpool@users.noreply.github.com>
2018-04-30 20:23:57 -0700

    V1.2 improved robustness and error handling
    - Added handling of failures on various VPS sustems
    - Existing Swap file handling
    - Added 'onlynet=ipv4' to reden.conf to reduce connection failures
    - maxconnections=64 (was 256) to reduce CPU/RAM utilization on certain nodes
    - increased delay netween stop/start for daemon to 60 sec
    - added diagnostics outputs and color-coding

--- reden-setup.sh
+++ reden-setup.sh
@@ -1,43 +1,51 @@
 #!/bin/bash
-# REDEN Masternode Setup Script V1.1 for Ubuntu 16.04 LTS
+# REDEN Masternode Setup Script V1.2 for Ubuntu 16.04 LTS
+# (c) 2018 by Allroad [FasterPool.com] for Reden 
 
-# Clears keyboard input buffer
+#Color codes
+RED='\033[0;91m'
+GREEN='\033[1;32m'
+YELLOW='\033[1;33m'
+NC='\033[0m' # No Color
+
+#Reden TCP port
+PORT=13058
+
+#Clear keyboard input buffer
 function clear_stdin { while read -r -t 0; do read -r; done; }
 
+#Delay script execution
+function delay { echo -e "${GREEN}Sleep for $1 seconds...${NC}"; sleep "$1"; }
+
 clear
-echo "Updating system and installing required packages..."
+echo -e "${YELLOW}REDEN Masternode Setup Script V1.2 for Ubuntu 16.04 LTS${NC}"
+echo -e "${GREEN}Updating system and installing required packages...${NC}"
 sudo apt-get update -y
 
-# Install dig if it's not present
+# Install dnsutils if necessary
 dpkg -s dnsutils 2>/dev/null >/dev/null || sudo apt-get -y install dnsutils
-
-echo "REDEN Masternode Setup Script V1.1 for Ubuntu 16.04 LTS"
 
 publicip=''
 publicip=$(dig +short myip.opendns.com @resolver1.opendns.com)
 
 if [ -n $publicip ]; then
-    echo "IP Address detected:" $publicip
+    echo -e "${YELLOW}IP Address detected:" $publicip ${NC}
 else
-    echo -e "ERROR: Public IP Address was not detected! \a"
+    echo -e "${RED}ERROR:${NC} Public IP Address was not detected! \a"
     clear_stdin
     read -e -p "Enter VPS Public IP Address: " publicip
 fi
-
-#Reden TCP port
-Port='13058'
 
 # update packages and upgrade Ubuntu
 sudo apt-get -y upgrade
 sudo apt-get -y dist-upgrade
 sudo apt-get -y autoremove
-sudo apt-get -y install wget nano htop git jq
+sudo apt-get -y install wget nano htop jq
 sudo apt-get -y install libzmq3-dev
 sudo apt-get -y install libboost-system-dev libboost-filesystem-dev libboost-chrono-dev libboost-program-options-dev libboost-test-dev libboost-thread-dev
 sudo apt-get -y install libevent-dev
 
 sudo apt -y install software-properties-common
-
 sudo add-apt-repository ppa:bitcoin/bitcoin -y
 sudo apt-get -y update
 sudo apt-get -y install libdb4.8-dev libdb4.8++-dev
@@ -53,23 +61,31 @@ sudo apt-get update -y
 sudo ufw default deny incoming
 sudo ufw default allow outgoing
 sudo ufw allow ssh
-sudo ufw allow $Port/tcp
+sudo ufw allow $PORT/tcp
+echo -e "${YELLOW}"
 sudo ufw --force enable
-
+echo -e "${NC}"
 
 #Generating Random Password for redend JSON RPC
 rpcpassword=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1)
 
 #Create 2GB swap file
-if [ ! -f /var/swap.img ]; then
-    
-    echo -e 'Creating 2GB disk swap file... This may take a few minutes! \a'
+if grep -q "SwapTotal" /proc/meminfo; then
+    echo -e "${GREEN}Swap is already configured${NC}"
+else
+    echo -e "${YELLOW}Creating 2GB disk swap file. \nThis may take a few minutes!${NC} \a"
     touch /var/swap.img
     chmod 600 swap.img
     dd if=/dev/zero of=/var/swap.img bs=1024k count=2000
-    mkswap /var/swap.img
-    swapon /var/swap.img
-    echo '/var/swap.img none swap sw 0 0' >> /etc/fstab 
+    mkswap /var/swap.img 2> /dev/null
+    swapon /var/swap.img 2> /dev/null
+    if [ $? -eq 0 ]; then
+        echo '/var/swap.img none swap sw 0 0' >> /etc/fstab
+        echo -e "${GREEN}Swap was created successfully!${NC}"
+    else
+        echo -e "${YELLOW}Operation not permitted! Optional swap was not created.${NC} \a"
+        rm /var/swap.img
+    fi
 fi
 
 #Installing Daemon
@@ -79,25 +95,33 @@ cd ~
 #sudo tar -xzvf reden_ubuntu16_1.0.0_linux.gz --strip-components 1 --directory /usr/bin
 #sudo rm reden_ubuntu16_1.0.0_linux.gz
 
-# Copy binaries to /usr/bin
-sudo cp RedenMasternodeSetup/Reden-v1.0-Ubuntu16.04/reden* /usr/bin/ > /dev/null
+#Stop daemon if it's already running
+if pgrep -x 'redend' > /dev/null; then
+    reden-cli stop
+    delay 30
+    if pgrep -x 'redend' > /dev/null; then
+        echo -e "${RED}redend daemon failed to stop!${NC} \a"
+        echo -e "${YELLOW}Attempting to kill redend${NC}"
+        pkill redend
+        delay 30
+    fi
+fi
 
+# Copy binaries to /usr/bin
+sudo cp RedenMasternodeSetup/Reden-v1.0-Ubuntu16.04/reden* /usr/bin/
 sudo chmod 755 -R ~/RedenMasternodeSetup
 sudo chmod 755 /usr/bin/reden*
 
-#Stop daemon if it's already running
-if pgrep -x 'redend' > /dev/null; then
-	reden-cli stop
-	echo 'sleep for 10 seconds...'
-	sleep 10
-fi
+# Copy masternode monitoring script
+cp ~/RedenMasternodeSetup/nodemon.sh /usr/local/bin
+sudo chmod 711 /usr/local/bin/nodemon.sh
 
 #Create reden.conf
 if [ ! -f ~/.redencore/reden.conf ]; then 
 	sudo mkdir ~/.redencore
 fi
 
-echo 'Creating reden.conf...'
+echo -e "${YELLOW}Creating reden.conf...${NC}"
 cat <<EOF > ~/.redencore/reden.conf
 rpcuser=redenrpc
 rpcpassword=$rpcpassword
@@ -107,22 +131,23 @@ sudo chmod 755 -R ~/.redencore/reden.conf
 
 #Starting daemon first time
 redend -daemon
-echo 'sleep for 10 seconds...'
-sleep 10
+delay 30
 
 #Generate masternode private key
-echo 'Generating masternode key...'
+echo -e "${YELLOW}Generating masternode key...${NC}"
 genkey=$(reden-cli masternode genkey)
 reden-cli stop
+delay 60
 
 cat <<EOF > ~/.redencore/reden.conf
 rpcuser=redenrpc
 rpcpassword=$rpcpassword
 rpcallowip=127.0.0.1
+onlynet=ipv4
 listen=1
 server=1
 daemon=1
-maxconnections=256
+maxconnections=64
 externalip=$publicip
 masternode=1
 masternodeprivkey=$genkey
@@ -132,35 +157,35 @@ EOF
 redend
 
 #Setting auto star cron job for redend
-echo 'Configuring crontab job...'
-cronjob='@reboot sleep 30 && redend'
+cronjob="@reboot sleep 30 && redend"
 crontab -l > tempcron
 if ! grep -q "$cronjob" tempcron; then
-	echo $cronjob >> tempcron
-	crontab tempcron
+    echo -e "${GREEN}Configuring crontab job...${NC}"
+    echo $cronjob >> tempcron
+    crontab tempcron
 fi
 rm tempcron
 
 echo -e "========================================================================
-Masternode setup is complete!
+${YELLOW}Masternode setup is complete!${NC}
 ========================================================================
 
-Masternode was installed with VPS IP Address: $publicip
+Masternode was installed with VPS IP Address: ${YELLOW}$publicip${NC}
 
-Masternode Private Key: $genkey
+Masternode Private Key: ${YELLOW}$genkey${NC}
 
 Now you can add the following string to the masternode.conf file
 for your Hot Wallet (the wallet with your Reden collateral funds):
 ======================================================================== \a"
-echo "mn1 $publicip:$Port $genkey TxId TxIdx"
-echo "========================================================================
+echo -e "${YELLOW}mn1 $publicip:$PORT $genkey TxId TxIdx${NC}"
+echo -e "========================================================================
 
 Use your mouse to copy the whole string above into the clipboard by
 tripple-click + single-click (Dont use Ctrl-C) and then paste it 
-into your masternodes.conf file and replace:
-    'mn1' - with your desired masternode name (alias)
-    'TxId' - with Transaction Id from masternode outputs
-    'TxIdx' - with Transaction Index (0 or 1)
+into your ${YELLOW}masternode.conf${NC} file and replace:
+    ${YELLOW}mn1${NC} - with your desired masternode name (alias)
+    ${YELLOW}TxId${NC} - with Transaction Id from masternode outputs
+    ${YELLOW}TxIdx${NC} - with Transaction Index (0 or 1)
      Remember to save the masternode.conf and restart the wallet!
 
 To introduce your new masternode to the Reden network, you need to
@@ -170,23 +195,23 @@ the collateral for this node is secured."
 clear_stdin
 read -p "*** Press any key to continue ***" -n1 -s
 
-echo "1) Wait for the node wallet on this VPS to sync with the other nodes
+echo -e "1) Wait for the node wallet on this VPS to sync with the other nodes
 on the network. Eventually the 'IsSynced' status will change
 to 'true', which will indicate a comlete sync, although it may take
 from several minutes to several hours depending on the network state.
 Your initial Masternode Status may read:
-    'Node just started, not yet activated' or
-    'Node  is not in masternode list', which is normal and expected.
+    ${YELLOW}Node just started, not yet activated${NC} or
+    ${YELLOW}Node  is not in masternode list${NC}, which is normal and expected.
 
 2) Wait at least until 'IsBlockchainSynced' status becomes 'true'.
 At this point you can go to your wallet and issue a start
 command by either using Debug Console:
-    Tools->Debug Console-> enter: masternode start-alias mn1
-    where 'mn1' is the name of your masternode (alias)
+    Tools->Debug Console-> enter: ${YELLOW}masternode start-alias mn1${NC}
+    where ${YELLOW}mn1${NC} is the name of your masternode (alias)
     as it was entered in the masternode.conf file
     
 or by using wallet GUI:
-    Masternodes -> Select masternode -> RightClick -> start alias
+    Masternodes -> Select masternode -> RightClick -> ${YELLOW}start alias${NC}
 
 Once completed step (2), return to this VPS console and wait for the
 Masternode Status to change to: 'Masternode successfully started'.
@@ -203,7 +228,7 @@ clear_stdin
 read -p "*** Press any key to continue ***" -n1 -s
 
 echo -e "
-...scroll up to see previous screens...
+${GREEN}...scroll up to see previous screens...${NC}
 
 
 Here are some useful commands and tools for masternode troubleshooting:
@@ -211,33 +236,35 @@ Here are some useful commands and tools for masternode troubleshooting:
 ========================================================================
 To view masternode configuration produced by this script in reden.conf:
 
-cat ~/.redencore/reden.conf
+${YELLOW}cat ~/.redencore/reden.conf${NC}
 
 Here is your reden.conf generated by this script:
----------------------------------------"
+-------------------------------------------------${YELLOW}"
 cat ~/.redencore/reden.conf
-echo -e "---------------------------------------
+echo -e "${NC}-------------------------------------------------
 
 NOTE: To edit reden.conf, first stop the redend daemon,
 then edit the reden.conf file and save it in nano: (Ctrl-X + Y + Enter),
 then start the redend daemon back up:
 
-to stop:   eden-cli stop
-to edit:   nano ~/.redencore/reden.conf
-to start:  edend
+to stop:   ${YELLOW}eden-cli stop${NC}
+to edit:   ${YELLOW}nano ~/.redencore/reden.conf${NC}
+to start:  ${YELLOW}edend${NC}
 ========================================================================
 To view Redend debug log showing all MN network activity in realtime:
 
-tail -f ~/.redencore/debug.log
+${YELLOW}tail -f ~/.redencore/debug.log${NC}
 ========================================================================
 To monitor system resource utilization and running processes:
 
-htop
+${YELLOW}htop${NC}
 ========================================================================
 To view the list of peer connections, status of your masternode, 
 sync status etc. in real-time, run the nodemon.sh script:
 
-bash ~/RedenMasternodeSetup/nodemon.sh
+${YELLOW}nodemon.sh${NC}
+
+or just type 'node' and hit <TAB> to autocomplete script name.
 ========================================================================
 
 
@@ -249,5 +276,7 @@ RCdYg5yq3YfymwrZi8EMBSFHxcwR7acniS
 ...and make sure to check back for updates!
 
 "
+# Run nodemon.sh
+nodemon.sh
 
-~/RedenMasternodeSetup/nodemon.sh
+# EOF
